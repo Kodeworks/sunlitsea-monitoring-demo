0e39f04b13a27a52d83a5bfc0a842ed1117be7d0
- <->
2020-06-17 11:42:09 +0200

    .

---
+++ precache-manifest.cd13a3aff73dbf111d085013ec38dae7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75306e41dbf645db71358bfa4aaafe0a",
+    "url": "/sunlitsea-monitoring-demo/index.html"
+  },
+  {
+    "revision": "975a14d767cc88096194",
+    "url": "/sunlitsea-monitoring-demo/static/css/main.e7ede54d.chunk.css"
+  },
+  {
+    "revision": "503d75f1f3f9af10f14b",
+    "url": "/sunlitsea-monitoring-demo/static/js/2.bf145c06.chunk.js"
+  },
+  {
+    "revision": "2ad625c0c4f50f26152b8b01b4038c30",
+    "url": "/sunlitsea-monitoring-demo/static/js/2.bf145c06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "975a14d767cc88096194",
+    "url": "/sunlitsea-monitoring-demo/static/js/main.6c8f82fa.chunk.js"
+  },
+  {
+    "revision": "433b3de0582177d7767f",
+    "url": "/sunlitsea-monitoring-demo/static/js/runtime-main.05930334.js"
+  }
+]);
