b718789347382711dae1c2c706b44b63369c003c
- <->
2020-06-17 11:32:45 +0200

    init from 7e45ceb0ac3fa67b73f2ce8b05cef1fee75f546f

---
+++ precache-manifest.cb86473e6b965b86d93f194cbaaac3bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d14229def44dfa53fdaf22e883255fca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b634523dc9ab7732d27",
+    "url": "/static/css/main.e7ede54d.chunk.css"
+  },
+  {
+    "revision": "503d75f1f3f9af10f14b",
+    "url": "/static/js/2.bf145c06.chunk.js"
+  },
+  {
+    "revision": "2ad625c0c4f50f26152b8b01b4038c30",
+    "url": "/static/js/2.bf145c06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b634523dc9ab7732d27",
+    "url": "/static/js/main.0ae87861.chunk.js"
+  },
+  {
+    "revision": "5c3affa5227ec3ca2850",
+    "url": "/static/js/runtime-main.0f7cfd52.js"
+  }
+]);
